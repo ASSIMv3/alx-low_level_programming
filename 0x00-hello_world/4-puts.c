35399565794735b38cdd42aaf83c4214525d5351
ASSIMv3 <hassanassim01@gmail.com>
2023-03-16 07:02:44 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- * main prints Programming is like building a multilingual puzzle.
- * return: 0
-**/
-
+ *  main - Print '"Programming is like building a multilingual puzzle'
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
