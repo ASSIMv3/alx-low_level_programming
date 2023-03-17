32d359f16ff78ae192a4640b89fc24d57fbd905b
ASSIMv3 <hassanassim01@gmail.com>
2023-03-17 05:27:11 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,10 @@
 int main(void)
 {
 	char ch;
-	for (ch=97; ch<123; ch++)
+
+	for (ch = 97; ch < 123; ch++)
 		putchar(ch);
-	for (ch=65; ch<91; ch++)
+	for (ch = 65; ch < 91; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
