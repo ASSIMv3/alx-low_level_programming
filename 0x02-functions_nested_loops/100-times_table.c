05b11c0468ed721c6cc613285836372927667d9f
ASSIMv3 <hassanassim01@gmail.com>
2023-03-21 08:09:18 -0700

    task100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_table -  a function that prints the n times table, starting with 0.
+ * print_times_table -  a function that prints the n times table
  * @n: int
  * Return: void
  */
