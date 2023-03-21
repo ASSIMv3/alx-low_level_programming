be679734dc49709eb3d695dcb48c72d1914438e5
ASSIMv3 <hassanassim01@gmail.com>
2023-03-21 05:13:20 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int c, l;
+
+	for (l = 0; l < 10; l++)
+	{
+		for (c = 97; c <= 122; c++)
+		{
+			_putchar(c);
+		}
+		putchar('\n');
+	}
+}
