decc785de15d94cf7eae994cfdc92b1634d8f3f3
ASSIMv3 <hassanassim01@gmail.com>
2023-03-23 04:46:35 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers Entry point
+ * print_numbers - entry point
  * Return: void
  */
 
 void print_numbers(void)
 {
-	int n;
+	int i;
 
-	for (n = 0; n < 10; n++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(n + '0');
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
