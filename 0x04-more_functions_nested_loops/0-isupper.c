0024ac1ffd5f56dd90f0b2712bf0fc96d9d2185e
ASSIMv3 <hassanassim01@gmail.com>
2023-03-23 04:30:02 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <sdtio>
 
 /**
- * isupper - checks for uppercase char
+ * _isupper - checks for uppercase char
  * @c: char
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
