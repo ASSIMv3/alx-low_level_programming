44ff705c8bf9d136c4ac0db682c10cbc82c00726
ASSIMv3 <hassanassim01@gmail.com>
2023-03-23 09:20:02 -0700

    task100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  calculate largest prime of 612852475143
+ * Return: Success Always
+ */
+int main(void)
+{
+	long i, j;
+
+	i = 612852475143;
+	for (j = 2; j < i; j++)
+	{
+		while (i % i == 0)
+			i =  / i;
+	}
+
+	printf("%lu\n", i);
+
+	return (0);
+}
