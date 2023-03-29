3d5a5bb814a216833fef99726eee6a2927072563
ASSIMv3 <hassanassim01@gmail.com>
2023-03-29 15:13:52 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates 2 strings
+ * @dest: char
+ * @src: char
+ * Return: destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i++] = src[j];
+
+	return (dest);
+}
