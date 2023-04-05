4308f4f08c14a83c379ea55f0f5e4b5b56c2e66a
ASSIMv3 <hassanassim01@gmail.com>
2023-04-05 07:27:01 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,13 +6,13 @@
  * @i: number
  * Return: square root
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt_root(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (_sqrt_recursion(n, i + 1));
+	return (_sqrt_root(n, i + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
