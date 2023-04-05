6fe2a52629b8316ea990d0988b0726aef01bbaa9
ASSIMv3 <hassanassim01@gmail.com>
2023-04-05 07:35:00 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,13 +6,13 @@
  * @i: divisor
  * Return: 1 if n is a prime, 0 if n isn't
  */
-int is_prime_number(int n, int i)
+int is_prime(int n, int i)
 {
 	if (n == i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (is_prime_number(n, i + 1));
+	return (is_prime(n, i + 1));
 }
 /**
  * is_prime_number - check ifthe number is a prime number
@@ -23,5 +23,5 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (is_prime_number(n, 2));
+	return (is_prime(n, 2));
 }
