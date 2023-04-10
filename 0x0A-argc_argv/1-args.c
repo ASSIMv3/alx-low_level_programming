4b044daff788a5a33d02f1dbfbf04d3e595ae094
ASSIMv3 <hassanassim01@gmail.com>
2023-04-10 08:04:15 -0700

    task12

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
