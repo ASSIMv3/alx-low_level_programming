ddac5e8a405f45081c239e43ca48a380e813b9bc
ASSIMv3 <hassanassim01@gmail.com>
2023-04-10 08:38:22 -0700

    task100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+	int coin_values[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		coins += cents / coin_values[i];
+		cents %= coin_values[i];
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
