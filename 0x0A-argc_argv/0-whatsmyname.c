f43a5599a64438bfee05d48fa0dd875f4f3d04a2
ASSIMv3 <hassanassim01@gmail.com>
2023-04-10 07:57:53 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints its name
- * @argc: argument count
- * @argv: argument vector
+ * main - print its name
+ * @argc: arguments count
+ * @argv: arguments vector
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(__attribute__ ((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
