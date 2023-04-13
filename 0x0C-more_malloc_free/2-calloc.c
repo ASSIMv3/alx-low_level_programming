40f556b3c081dad02226efe57452d55ee9f9babb
ASSIMv3 <hassanassim01@gmail.com>
2023-04-13 13:14:31 -0700

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,21 +9,22 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
+	char *ptr;
+
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	void *ptr = malloc(nmemb * size);
-
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
-	unsigned char *p = ptr;
-
-	for (size_t i = 0; i < nmemb * size; i++)
+	p = ptr;
+	for (i = 0; i < nmemb * size; i++)
 	{
 		*p++ = 0;
 	}
