d1d9e84142aafe75418bcc5ac64d654cbabf04e0
ASSIMv3 <hassanassim01@gmail.com>
2023-04-13 13:04:50 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,32 +11,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int i, j, len1, len2;
+	char *result;
+	unsigned int i, j, len1, len2;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    len1 = strlen(s1);
-    len2 = strlen(s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
-    if (n > len2)
-        n = len2;
+	if (n > len2)
+		n = len2;
 
-    result = malloc(sizeof(char) * (len1 + n + 1));
+	result = malloc(sizeof(char) * (len1 + n + 1));
 
-    if (result == NULL)
-        return (NULL);
+	if (result == NULL)
+	return (NULL);
 
-    for (i = 0; i < len1; i++)
-        result[i] = s1[i];
+	for (i = 0; i < len1; i++)
+	result[i] = s1[i];
 
-    for (j = 0; j < n; j++)
-        result[i + j] = s2[j];
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
 
-    result[i + j] = '\0';
+	result[i + j] = '\0';
 
-    return (result);
+	return (result);
 }
