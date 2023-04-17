9be06c0d5275dad4647a83c096ab99ce3ea8d740
ASSIMv3 <hassanassim01@gmail.com>
2023-04-17 09:57:24 -0700

    task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,11 +5,11 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner
- * Return: 0
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
