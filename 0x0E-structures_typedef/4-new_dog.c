cfc61da25f217398dfad28357194672081b63426
ASSIMv3 <hassanassim01@gmail.com>
2023-04-17 11:12:02 -0700

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,35 +11,43 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    if (!name || !owner) {
-        return NULL;
-    }
-    
-    size_t name_len = strlen(name);
-    size_t owner_len = strlen(owner);
-    
-    dog_t *new_dog = malloc(sizeof(dog_t));
-    if (!new_dog) {
-        return NULL;
-    }
-    
-    new_dog->name = malloc(name_len + 1);
-    if (!new_dog->name) {
-        free(new_dog);
-        return NULL;
-    }
-    strcpy(new_dog->name, name);
-    
-    new_dog->owner = malloc(owner_len + 1);
-    if (!new_dog->owner) {
-        free(new_dog->name);
-        free(new_dog);
-        return NULL;
-    }
-    strcpy(new_dog->owner, owner);
-    
-    new_dog->age = age;
-    
-    return new_dog;
-}
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	int name_len = 0, owner_len = 0;
+	char *name_copy, *owner_copy;
+
+	while (name[name_len] != '\0')
+		name_len++;
+	while (owner[owner_len] != '\0')
+		owner_len++;
+
+	name_copy = malloc(sizeof(char) * (name_len + 1));
+	owner_copy = malloc(sizeof(char) * (owner_len + 1));
+
+	if (name_copy == NULL || owner_copy == NULL)
+	{
+		free(name_copy);
+		free(owner_copy);
+		free(new_dog);
+		return (NULL);
+	}
+
+	for (int i = 0; i < name_len; i++)
+		name_copy[i] = name[i];
+	name_copy[name_len] = '\0';
+	for (int i = 0; i < owner_len; i++)
+		owner_copy[i] = owner[i];
+	owner_copy[owner_len] = '\0';
+
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+
+	return (new_dog);
+}
