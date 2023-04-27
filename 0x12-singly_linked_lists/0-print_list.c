3ef53861009325181a9d9148804c096b48fa2b7f
ASSIMv3 <hassanassim01@gmail.com>
2023-04-27 12:54:15 -0700

    task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head of the list_t list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] ", h->len);
+		if (h->str == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
