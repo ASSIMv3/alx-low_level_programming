44c761af008a95e1c93b4949b40d75681c648fa3
ASSIMv3 <hassanassim01@gmail.com>
2023-04-30 02:53:59 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,39 +9,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *current, *new_node;
+	listint_t *new_node, *current;
+	unsigned int i;
 
 	if (head == NULL)
 		return (NULL);
 
-	current = *head;
-
-	while (i < idx - 1)
-	{
-		if (current == NULL)
-			return (NULL);
-
-		current = current->next;
-		i++;
-	}
-
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
+		return (new_node);
 	}
-	else
+
+	current = *head;
+	i = 0;
+
+	while (i < idx && current != NULL)
 	{
-		new_node->next = current->next;
-		current->next = new_node;
+		if (i == idx - 1)
+		{
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+		current = current->next;
+		i++;
 	}
-	return (new_node);
+
+	free(new_node);
+	return (NULL);
 }
