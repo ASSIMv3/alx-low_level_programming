66746b9dfbc0bb0ea01aac29e11a0a91a3376fdc
ASSIMv3 <hassanassim01@gmail.com>
2023-05-04 14:02:05 -0700

    task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string of 0 and 1 chars
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int uint_num = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		uint_num <<= 1;
+		uint_num += (*b - '0');
+		b++;
+	}
+
+	return (uint_num);
+}
+
