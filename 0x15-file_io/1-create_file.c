ef048f174ce44c9a283e1d6b1c2d31a5f4db1297
ASSIMv3 <hassanassim01@gmail.com>
2023-05-08 07:03:37 -0700

    task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to the file
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, ret, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len])
+			len++;
+		ret = write(fd, text_content, len);
+		if (ret == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
