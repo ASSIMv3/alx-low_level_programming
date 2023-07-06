b8cc56a7701a07671b73d7b67cef42d2374c5d37
ASSIMv3 <hassanassim01@gmail.com>
2023-07-06 10:34:44 -0700

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieve thevalue associated with a key from the hash table
+ * @ht: The hash table to search
+ * @key: The key to search for
+ *
+ * Return: The value associated with the key, or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	/* Traverse the linked list at the index */
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
